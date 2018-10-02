1ace0b4877111645640d4a4c5389a415eb101acd
Angel cardillo <gastoncardillo2014@outlook.com>
2018-10-02 01:44:22 -0300

    Escritorio nuevo + Modificaciones en la cadena de conexion + Modificacion en inicio de app

---
+++ AerolineasSaleTarde/AerolineasSaleTarde/Formularios/Escritorio/Escritorio.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Public Property actual As Form
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        actual = deletongo
+
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Panel3.Visible = False
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Panel3.Visible = False
+
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Panel3.Visible = True
+    End Sub
+End Class
