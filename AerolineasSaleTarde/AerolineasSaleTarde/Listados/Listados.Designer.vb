d25ef0cc58fd63d562e43a0a9366b39008a9f3ff
angel cardillo <gastoncardillo2014@outlook.com>
2018-10-19 20:05:02 -0300

    Primer listado, falta query

---
+++ AerolineasSaleTarde/AerolineasSaleTarde/Listados/Listados.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Listados
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ReportDataSource2 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Me.rv = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.PasajeroAbordoBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DataSet1 = New AerolineasSaleTarde.DataSet1()
+        Me.btn_buscarVuelo = New System.Windows.Forms.Button()
+        Me.cmb_origen = New AerolineasSaleTarde.CMB_01()
+        Me.cmb_destino = New AerolineasSaleTarde.CMB_01()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        CType(Me.PasajeroAbordoBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'rv
+        '
+        ReportDataSource2.Name = "DataSet1"
+        ReportDataSource2.Value = Me.PasajeroAbordoBindingSource
+        Me.rv.LocalReport.DataSources.Add(ReportDataSource2)
+        Me.rv.LocalReport.ReportEmbeddedResource = "AerolineasSaleTarde.PasajerosAbordo.rdlc"
+        Me.rv.Location = New System.Drawing.Point(12, 118)
+        Me.rv.Name = "rv"
+        Me.rv.ServerReport.BearerToken = Nothing
+        Me.rv.Size = New System.Drawing.Size(759, 297)
+        Me.rv.TabIndex = 0
+        '
+        'PasajeroAbordoBindingSource
+        '
+        Me.PasajeroAbordoBindingSource.DataMember = "PasajeroAbordo"
+        Me.PasajeroAbordoBindingSource.DataSource = Me.DataSet1
+        '
+        'DataSet1
+        '
+        Me.DataSet1.DataSetName = "DataSet1"
+        Me.DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'btn_buscarVuelo
+        '
+        Me.btn_buscarVuelo.Location = New System.Drawing.Point(413, 28)
+        Me.btn_buscarVuelo.Name = "btn_buscarVuelo"
+        Me.btn_buscarVuelo.Size = New System.Drawing.Size(75, 23)
+        Me.btn_buscarVuelo.TabIndex = 19
+        Me.btn_buscarVuelo.Text = "Buscar"
+        Me.btn_buscarVuelo.UseVisualStyleBackColor = True
+        '
+        'cmb_origen
+        '
+        Me.cmb_origen.FormattingEnabled = True
+        Me.cmb_origen.Location = New System.Drawing.Point(72, 28)
+        Me.cmb_origen.Name = "cmb_origen"
+        Me.cmb_origen.nombre_campo = Nothing
+        Me.cmb_origen.Size = New System.Drawing.Size(121, 21)
+        Me.cmb_origen.TabIndex = 18
+        Me.cmb_origen.validable = True
+        '
+        'cmb_destino
+        '
+        Me.cmb_destino.FormattingEnabled = True
+        Me.cmb_destino.Location = New System.Drawing.Point(258, 28)
+        Me.cmb_destino.Name = "cmb_destino"
+        Me.cmb_destino.nombre_campo = Nothing
+        Me.cmb_destino.Size = New System.Drawing.Size(121, 21)
+        Me.cmb_destino.TabIndex = 17
+        Me.cmb_destino.validable = True
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(208, 28)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(43, 13)
+        Me.Label14.TabIndex = 15
+        Me.Label14.Text = "Destino"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(27, 25)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(38, 13)
+        Me.Label13.TabIndex = 14
+        Me.Label13.Text = "Origen"
+        '
+        'Listados
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btn_buscarVuelo)
+        Me.Controls.Add(Me.cmb_origen)
+        Me.Controls.Add(Me.cmb_destino)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.rv)
+        Me.Name = "Listados"
+        Me.Text = "Listados"
+        CType(Me.PasajeroAbordoBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents rv As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents PasajeroAbordoBindingSource As BindingSource
+    Friend WithEvents DataSet1 As DataSet1
+    Friend WithEvents btn_buscarVuelo As Button
+    Friend WithEvents cmb_origen As CMB_01
+    Friend WithEvents cmb_destino As CMB_01
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label13 As Label
+End Class
---
+++ AerolineasSaleTarde/AerolineasSaleTarde/Listados/Listados.vb
@@ -0,0 +1,23 @@
+﻿Public Class Listados
+    Dim _conex As New BD_TRANSACCIONAL
+    Private Sub Listados_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        Me.rv.RefreshReport()
+    End Sub
+
+    Private Sub btn_buscarVuelo_Click(sender As Object, e As EventArgs) Handles btn_buscarVuelo.Click
+        If cmb_origen.SelectedIndex <> -1 And cmb_destino.SelectedIndex <> -1 Then
+            Dim sql As String = " SET DATEFORMAT DMY SELECT * from Vuelos v Where idAereopuertoOrigen = " & cmb_origen.SelectedValue & " and idAereopuertoDestino = " & cmb_destino.SelectedValue & " and '" & Today.Day & "' = v.fechaSalida"
+            Dim tabla As New DataTable
+            tabla = Me._conex.consultaATabla(sql)
+            If tabla.Rows.Count = 0 Then
+                MsgBox("No hay vuelos disponibles ya embarcados")
+            Else
+
+            End If
+        Else
+            MsgBox("Falta un dato")
+        End If
+    End Sub
+End Class
